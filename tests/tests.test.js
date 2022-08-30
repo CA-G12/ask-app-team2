f17118079b13aabba53fef12ca68403fd97cf2cf
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-30 19:54:17 +0300

    Added commented test close #18

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,5 +1,21 @@
-/* eslint-disable no-undef */
+// const connection = require('../src/database/config/connection');
+// const build = require('../src/database/config/build');
 
+/* eslint-disable no-undef */
 test('initial', () => {
   expect(3).toBe(3);
 });
+
+// beforeAll(() => build());
+// afterAll(() => connection.end());
+
+// describe('Testing development database', () => {
+//   test('Testing the first parameter of the returned object', () => {
+//     connection
+//       .query('SELECT id, content FROM questions')
+//       .then((data) => {
+//         console.log(data);
+//         return expect(Object.keys(data.rows[0]).length).toBe(2);
+//       });
+//   });
+// });
