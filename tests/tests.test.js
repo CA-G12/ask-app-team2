4c2e7cddc41f057e9e99a3a84c989ab83cceee72
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-30 21:26:23 +0300

    Edited some bugs close #18

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,13 +1,16 @@
 const supertest = require('supertest');
 
 const app = require('../src/app');
-// const connection = require('../src/database/config/connection');
-// const build = require('../src/database/config/build');
+const build = require('../src/database/config/build');
+const connection = require('../src/database/config/connection');
 
 /* eslint-disable no-undef */
 test('initial', () => {
   expect(3).toBe(3);
 });
+
+beforeEach(() => build());
+afterAll(() => connection.end());
 
 describe('Testing "/api/v1/signup" route', () => {
   it('test the returned object', (done) => {
@@ -29,17 +32,3 @@ describe('Testing "/api/v1/signup" route', () => {
       });
   });
 });
-
-// beforeAll(() => build());
-// afterAll(() => connection.end());
-
-// describe('Testing development database', () => {
-//   test('Testing the first parameter of the returned object', () => {
-//     connection
-//       .query('SELECT id, content FROM questions')
-//       .then((data) => {
-//         console.log(data);
-//         return expect(Object.keys(data.rows[0]).length).toBe(2);
-//       });
-//   });
-// });
