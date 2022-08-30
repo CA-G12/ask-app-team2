529b61d26ee310a5239c30e8a596869bafec2e77
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-30 19:54:48 +0300

    Added the singup query and form validation close #18

---
+++ src/database/queries/signup.js
@@ -0,0 +1,13 @@
+const connection = require('../config/connection');
+
+const addUser = ({
+  username, fname, lname, password, email,
+}) => {
+  const sql = {
+    text: 'INSERT INTO users(username, fname, lname, password, email) values ($1, $2, $3, $4, $5)',
+    values: [username, fname, lname, password, email],
+  };
+  return connection.query(sql);
+};
+
+module.exports = addUser;
