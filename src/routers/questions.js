2373aeee61a74fb0b8c79dbc6b6e1904ce33085f
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-31 16:32:08 +0300

    Edited the quesiotn route to server the new name of the function close #14

--- src/routers/questions.js
+++ src/routers/questions.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { getQuestion } = require('../controllers');
 
-router.get('/', getQuestion);
+const { getQuestionController } = require('../controllers');
+const verifyAccessToken = require('./middlewares/verifyAccessToken');
+
+router.get('/', verifyAccessToken, getQuestionController);
 module.exports = router;
