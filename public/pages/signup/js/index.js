5d8cad03c36400a69864d3999e3da0fdbd673b4d
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2022-08-31 21:39:09 +0300

    edit relates 43

--- public/pages/signup/js/index.js
+++ public/pages/signup/js/index.js
@@ -4,7 +4,10 @@ const lastname = document.getElementById('lastname');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmpassword');
 const phone = document.getElementById('phone');
+const avatr = document.getElementById('avatar');
+const cover = document.getElementById('cover');
 const url = '/api/v1/auth/signup';
+
 fetch(url, {
   method: 'POST',
 
@@ -13,7 +16,7 @@ fetch(url, {
   },
   body: JSON.stringify({
     // eslint-disable-next-line max-len
-    username: username.value, firstname: firstname.value, lastname: lastname.value, password: password.value, confirmpassword: confirmPassword.value, phone: phone.value,
-  }),
-})
-  .then((data) => console.log(data));
+    username: username.value, firstname: firstname.value, lastname: lastname.value, password: password.value, confirmpassword: confirmPassword.value, phone: phone.value, cover: cover.value, avatr: avatr.value,
+  }).then((data) => { console.log(data); })
+    .catch((error) => console.log(error)),
+});
